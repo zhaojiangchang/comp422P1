04a0d37e84a714740da3929cf29e21507810a5f7
zhaojiang chang <zhaojiangchangnz@gmail.com>
2016-08-03 01:57:21 +1200

    remove non used files, create preprocessing.java (remove individule task)

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,31 @@
 
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import javax.swing.JScrollPane;
 
-import Q1.EdgeDetection;
-import Q1.ImageEnhancement;
-import Q1.NoiseCancellation;
-import Q2.MiningImageData;
-import Test.frmImageEnhancer;
+
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-//		final UI app = new UI();
+		BufferedImage outputImg;
 
+		String file = "hubble.tif";
+		String filterMethod = "mining";
+		Preprocessing  preprocessing  =  new Preprocessing (file);
+		if(filterMethod.equalsIgnoreCase("edge")){
+			Preprocessing  p  =  new Preprocessing (file);
+			BufferedImage img = p.filterImage("median");
+			preprocessing.setInputImg(img);
+			outputImg = preprocessing.filterImage(filterMethod);
+		}else{
+			outputImg = preprocessing.filterImage(filterMethod);
+		}
+		File outputfile = new File("result/filter-"+file);
+		ImageIO.write(outputImg,"tif", outputfile);
 
-		//Project Question 1
-		
-		//1.1 Edge detection
-		EdgeDetection edgeDetection =  new EdgeDetection("test-pattern.tif");
-		BufferedImage outputImgEdgeDetction = edgeDetection.processImage();
-		File outputfileEd = new File("result/q1/test-pattern-EdgeDetection.tif");
-		ImageIO.write(outputImgEdgeDetction,"tif", outputfileEd);
-
-		//1.2 noise cancellation cnvolution filter
-		NoiseCancellation noiseCancellation =  new NoiseCancellation("ckt-board-saltpep.tif");
-		BufferedImage outputImgNoiseConvFilter = noiseCancellation.meanFilter();
-		File outputfileNc = new File("result/q1/ckt-board-saltpep-noisecancellation-convolution-filter.tif");
-		ImageIO.write(outputImgNoiseConvFilter,"tif", outputfileNc);
-
-		//noise cancellation median filter
-		BufferedImage outputImgNoiseMedianFilter = noiseCancellation.medianFilter();
-		outputfileNc = new File("result/q1/ckt-board-saltpep-noisecancellation-median-filter.tif");
-		ImageIO.write(outputImgNoiseMedianFilter,"tif", outputfileNc);
-
-		//1.3 image enhancement
-		ImageEnhancement  imageEnhancement  =  new ImageEnhancement ("blurry-moon.tif");
-		BufferedImage outputImgImageEnhancement = imageEnhancement.enhancement();
-		File outputfileImageEnhancement = new File("result/q1/blurry-moon-enhancement.tif");
-		ImageIO.write(outputImgImageEnhancement,"tif", outputfileImageEnhancement);
-		
-		//Project Question 2
-		//2.1 mining space image
-		MiningImageData  miningImageData  =  new MiningImageData ("hubble.tif");
-				BufferedImage outputImgMiningImageData = miningImageData.meanFilter();
-				File outputfileMiningImageData = new File("result/q2/maining-image-data.tif");
-				ImageIO.write(outputImgMiningImageData,"tif", outputfileMiningImageData);
 	}
 	public Main(){
 
